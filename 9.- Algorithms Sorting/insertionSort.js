d9e2bb06c8e2bfbfeac1e5683f1a5a1d9e11188c
Fernando Salas <luisfernandosalasg@gmail.com>
2021-07-17 11:33:34 -0500

    Add Sorting Part1

---
+++ 9.- Algorithms Sorting/insertionSort.js
@@ -0,0 +1,36 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+/* function insertionSort(array) {
+	const length = array.length;
+	for (let i = 0; i < length; i++) {
+		if (array[i] < array[0]) {
+			//move number to the firt position
+			array.unshift(array.splice(i, 1)[0]);
+		} else {
+			for (let j = 1; j < i; j++) {
+				if (array[i] > array[j - 1] && array[i] < array[j]) {
+					//move number to the right sopt
+					array.splice(j, 0, array.splice(i, 1)[0]);
+				}
+			}
+		}
+	}
+}
+ */
+
+function insertionSort2(array) {
+	let length = array.length;
+	for (let i = 1; i < length; i++) {
+		let temp = array[i];
+		let j = i - 1;
+		while (j >= 0 && array[j] > temp) {
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = temp;
+	}
+}
+
+/* insertionSort2(numbers);
+numbers;
+ */
